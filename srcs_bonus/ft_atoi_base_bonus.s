15234e031e4acff96342dfddad012f5b57b9dcbf
Oscar Moreno Sancho <oscar.moreno.sancho@gmail.com>
2024-08-08 13:47:47 +0200

    start impl ft_atoi_base

--- srcs_bonus/ft_atoi_base_bonus.s
+++ srcs_bonus/ft_atoi_base_bonus.s
@@ -1,12 +1,32 @@
 ; Call convention arguments RDI, RSI, RDX, RCX, R8 y R9
 ; Return on RAX
-; RDI destination pointer
-; RSI source pointer
+; int ft_atoi_base(char *str, char *base)
+; RDI source string pointer
+; RSI base pointer
 ; RAX Result Return
 
+extern	ft_strlen
 global ft_atoi_base                       ; A global label to be declared for the linker ( GNU LD )
 
 section .text                           ; Section to put code
+
+ft_validate_base:
+
+ret
+
+ft_trim_spaces:
+
+ret
+
+ft_get_sign:
+
+ret
+
+ft_atoui_base:
+
+ret
+
+
 ft_atoi_base:
 	push	rdx
 	xor		rax, rax					; set to 0 by xoring with itself
