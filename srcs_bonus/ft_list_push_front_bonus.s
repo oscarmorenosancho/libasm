2fdb1cb9b4b891a48e34f46dfca9a4efeb9051c2
Oscar Moreno Sancho <oscar.moreno.sancho@gmail.com>
2024-08-11 16:00:19 +0200

    impl ft_list_push_front

--- srcs_bonus/ft_list_push_front_bonus.s
+++ srcs_bonus/ft_list_push_front_bonus.s
@@ -5,35 +5,43 @@
 ; RAX Result Return
 
 section .text                           ; Section to put code
+	extern malloc
 	global ft_list_push_front                       ; A global label to be declared for the linker ( GNU LD )
 	global ft_create_elem                       ; A global label to be declared for the linker ( GNU LD )
 
+; RDI	contains pointer to data to store to as content
+; RAX	pointer to list node created or NULL if allocation failed
 ft_create_elem:
-	push	rdx
-	xor		rax, rax					; set to 0 by xoring with itself
-.loop:
-	mov		dl, BYTE [rsi + rax]
-	mov		BYTE [rdi + rax], dl
-	cmp		dl, 0
-	je		.end
-	inc		rax
-	jmp		.loop
+	push	rdi									; save content pointer for later
+	mov		rdi, 16								; the size of node is 2 pointers so 16b
+	call	malloc
+	pop		rdi									; recover content into rdi
+	cmp		rax, 0								; if malloc returns NULL
+	je		.end								; than allocation failed, so get out
+	mov		qword [rax], rdi					; set content of node point to data
+	mov		qword [rax + 8], 0					; set pointer next to NULL
 .end:
-	mov		rax, rdi
-	pop		rdx
     ret
 	
-ft_atoi_base:
-	push	rdx
-	xor		rax, rax					; set to 0 by xoring with itself
-.loop:
-	mov		dl, BYTE [rsi + rax]
-	mov		BYTE [rdi + rax], dl
-	cmp		dl, 0
-	je		.end
-	inc		rax
-	jmp		.loop
+; RDI	contains pointer begin list
+; RSI	contains pointer to data to store to as content
+; RAX	pointer to list node created or NULL if allocation failed
+ft_list_push_front:
+	cmp		rdi, 0								; if begin list is NULL
+	je		.end								; just end (can't add to nowhere)
+	push	rdi									; save 1st arg o stack
+	mov		rdi, rsi							; set content for node creation
+	call	ft_create_elem
+	cmp		rax, 0
+	je		.end								; if node creation failed, just abort
+	mov		rdi, [rsp]							; recover list poiter from last stack
+	mov		rsi, [rdi]							; dereference 1st node of list, into rsi
+	cmp		rsi, 0								; if list is empty list begins with NULL
+	je		.point_first						; nothing to set as next in new node
+	mov		[rax + 8], rdi						; set next to previuos list begin
+.point_first
+	mov		[rdi], rax							; point new created element by list begin							
+.abort:
+	pop		rdi									; recover 1st arg into register
 .end:
-	mov		rax, rdi
-	pop		rdx
     ret
