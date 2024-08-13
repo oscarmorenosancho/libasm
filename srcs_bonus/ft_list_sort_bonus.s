d8f66c911288788d28a309ac97c32054e2018ae7
Oscar Moreno Sancho <oscar.moreno.sancho@gmail.com>
2024-08-13 19:14:30 +0200

    apparenty ft_list_search_pos aproves

--- srcs_bonus/ft_list_sort_bonus.s
+++ srcs_bonus/ft_list_sort_bonus.s
@@ -22,23 +22,21 @@ section .text                           ; Section to put code
 ft_list_search_pos:
 	mov		rax, rdi					; init rax with begin_list
 	cmp		rax, 0						; if begin list is NULL
-	jne		.end						
+	je		.end						
 	push	r8
 	push	rcx
 	push	rdx
 	push	rsi
 	push	rdi
-	;push	rbp
-	;mov		rbp, rsp
+	push	rbp
+	mov		rbp, rsp
 	;sub		rsp, 0						;reserve 0 bytes for local variables
 .loop:
 	mov		rcx, QWORD [rax]				; get first node of list into rcx
-	cmp		rcx, 0						; if node is NULL
-	jne		.break_loop
-	;mov		rdx, QWORD [rbp + 24]				; recover original value of rdx form the stack (cmp)
-	;mov		rsi, QWORD [rbp + 16]				; recover original value of rsi form the stack (ref_data)
-	mov		rdx, QWORD [rbp + 16]				; recover original value of rdx form the stack (cmp)
-	mov		rsi, QWORD [rbp + 8]				; recover original value of rsi form the stack (ref_data)
+	cmp		rcx, 0							; if node is NULL
+	je		.break_loop
+	mov		rdx, QWORD [rbp + 24]				; recover original value of rdx form the stack (cmp)
+	mov		rsi, QWORD [rbp + 16]				; recover original value of rsi form the stack (ref_data)
 	mov		rdi, QWORD [rcx]					; obtain data from node to compare
 	push	rcx
 	push	rax
@@ -46,14 +44,14 @@ ft_list_search_pos:
 	mov		rdi, rax					; move result to rdi
 	pop		rax							; recover rax where the cur list is 
 	pop		rcx							; recover rcx node
-	cmp		rdi, 0
-	jg		.break_loop
+	cmp		di, 0
+	jge		.break_loop
 	mov		rax, rcx					; set the cur list head to next in node
 	add		rax, 8						; set the cur list head to next in node
 	jmp		.loop
 .break_loop:
-	;mov		rsp, rbp
-	;pop		rbp
+	mov		rsp, rbp
+	pop		rbp
 	pop		rdx
 	pop		rsi
 	pop		rdi
--- tests/_bonus/test_list_search_pos.c
+++ tests/_bonus/test_list_search_pos.c
@@ -9,10 +9,15 @@
 int test_list_search_pos(t_list **begin_list, char *data_ref, int (*cmp)())
 {
 	t_list	**lr;
+	t_list	*n = NULL;
+
 	printf(GRN_COL"Using ft_list_search_pos for args list begin: %p\n"RST_COL, begin_list);
-	printf(GRN_COL"ref_data: \"%s\" cmp: %p\n"RST_COL, data_ref, cmp);
+	printf(GRN_COL"ref_data: \"%s\" (%p) cmp: %p\n"RST_COL, data_ref, data_ref, cmp);
 	lr = ft_list_search_pos(begin_list, data_ref, cmp);
-	printf("\tafter calling ft_list_search_pos, front list to insert %p\n", lr);
+	if (lr) n = *lr;  
+	printf("\tafter calling ft_list_search_pos, front list to insert: %p, node: %p\n", lr, n);
+	if (n)
+		printf("\tdata: \"%s\", next: %p\n", (char*)n->data, n->next);
 	return (0);
 }
 
@@ -34,25 +39,25 @@ int		test_list_search_pos_examples()
 	// printf(GRN_COL"\nCreate an empty list"RST_COL"\n");
 	// test_list_search_pos(NULL, NULL, strcmp);
 
-	printf(GRN_COL"\nCreate a list with one node"RST_COL"\n");
-	ft_list_push_front(&l, "static data");
-	test_list_search_pos(&l, "", strcmp);
+	// printf(GRN_COL"\nCreate a list with one node"RST_COL"\n");
+	// ft_list_push_front(&l, "static data");
+	// test_list_search_pos(&l, "", strcmp);
 
-	printf(GRN_COL"\nClear the list with one node"RST_COL"\n");
-	// ft_list_remove_front(&l, NULL);
-	ft_list_remove_if(&l, "", always_equal, NULL);	
-	test_list_search_pos(&l, "", strcmp);
+	// printf(GRN_COL"\nClear the list with one node"RST_COL"\n");
+	// ft_list_remove_if(&l, "", always_equal, NULL);	
+	// test_list_search_pos(&l, "", strcmp);
 
 	printf(GRN_COL"\nCreate a list with %d nodes"RST_COL"\n", number);
-	for (i = 0; i < number; i++)
+	for (i = number - 1; i >= 0; i--)
 	{
-		sprintf(buf, "content of node %d", i);
+		sprintf(buf, "%05d", i);
 		content = strdup(buf);
 		ft_list_push_front(&l, content);
+		printf(GRN_COL"\tpush node %p, data:\"%s\", next:%p"RST_COL"\n", l, (char*)l->data, l->next);
 	}
 	for (i = 0; i < number; i++)
 	{
-		sprintf(buf, "content of node %d", i);
+		sprintf(buf, "%05d", i);
 		test_list_search_pos(&l, buf, strcmp);
 	}
 	printf(GRN_COL"\nClear the list with %d nodes"RST_COL"\n", number);
